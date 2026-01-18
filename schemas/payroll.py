438f85155aed1a032cf4ad7ff8e51792aa04d5d4
siyabdev <siyabwebdev@gmail.com>
2026-01-18 18:17:57 +0500

    Payroll delete schema changed to accept payroll_id (instead of id) to avoid frontend confusion.

--- schemas/payroll.py
+++ schemas/payroll.py
@@ -71,7 +71,7 @@ class UpdatePayrollRequest:
 #Delete payroll(class) request
 class DeletePayrollRequest:
     def __init__(self, data):
-        self.id = data.get("id")
+        self.id = data.get("payrol_id")
     
     def is_valid(self):
         if not (self.id):
