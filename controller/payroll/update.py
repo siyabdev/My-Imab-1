d6122fd2e7800e817122761a104ae07c75602117
siyabdev <siyabwebdev@gmail.com>
2026-02-10 12:31:30 +0500

    Updates in Payroll

--- controller/payroll/update.py
+++ controller/payroll/update.py
@@ -3,18 +3,7 @@ from crud.payroll.update import update_payroll_crud
 from sqlalchemy.exc import IntegrityError
 from schemas.payroll import UpdatePayrollRequest, PayrollResponse
 from auth import require_auth
-from utils.utils import (
-    verify_payroll,
-    get_hourly_rate,
-    get_over_below,
-    get_score,
-    get_addition,
-    get_deduction,
-    get_gross,
-    get_tax,
-    get_total_net,
-    get_net_orion
-)
+from utils.utils import verify_payroll
 
 payroll_update_bp = Blueprint("payroll_update_bp", __name__, url_prefix="/payroll")
 
@@ -47,42 +36,15 @@ def update_payroll():
             "message": f"Payroll {payroll} not found."
             }), 404
 
-    #Calculations
-    contract_hours = payroll.employee_contract_hours
-    rota_hours = payroll.employee_rota_hours
-    worked_hours = payroll.employee_worked_hours
-    lates = payroll.employee_lates
-    early = payroll.employee_early
-    leaves = payroll.employee_leaves
-
-    employee_hourly_rate = get_hourly_rate(payroll.employee_basic_salary, contract_hours)
-    employee_over_below = get_over_below(worked_hours, contract_hours)
-    employee_score = get_score(lates, early, leaves)
-    total_addition = get_addition(employee_over_below, employee_hourly_rate)
-    total_deduction = get_deduction(employee_over_below, employee_hourly_rate)
-    total_gross = get_gross(payroll.employee_basic_salary, total_addition, total_deduction)
-    total_tax = get_tax(total_gross)
-    employee_total_net = get_total_net(total_gross, total_tax)
-    total_net_orion = get_net_orion(total_gross)
-
     try:
         updated_payroll = update_payroll_crud(
-        id=data.id,
-        employee_contract_hours=contract_hours,
-        employee_rota_hours=rota_hours,
-        employee_worked_hours=worked_hours,
-        employee_lates=lates,
-        employee_early=early,
-        employee_leaves=leaves,
-        employee_hourly_rate=employee_hourly_rate,
-        employee_over_below=employee_over_below,
-        employee_score=employee_score,
-        total_addition=total_addition,
-        total_deduction=total_deduction,
-        total_gross=total_gross,
-        total_tax=total_tax,
-        employee_total_net=employee_total_net,
-        total_net_orion=total_net_orion
+        id = data.id,
+        employee_contract_hours = data.employee_contract_hours,
+        employee_rota_hours = data.employee_rota_hours,
+        employee_worked_hours = data.employee_worked_hours,
+        employee_lates = data.employee_lates,
+        employee_early = data.employee_early,
+        employee_leaves = data.employee_leaves
     )
 
         current_app.logger.info(f"Payroll updated {updated_payroll}.")
--- crud/payroll/update.py
+++ crud/payroll/update.py
@@ -1,7 +1,18 @@
 from flask import current_app
 from database import db
-from utils.utils import verify_payroll
 from sqlalchemy.exc import IntegrityError
+from utils.utils import (
+    verify_payroll,
+    get_hourly_rate,
+    get_over_below,
+    get_score,
+    get_addition,
+    get_deduction,
+    get_gross,
+    get_tax,
+    get_total_net,
+    get_net_orion
+)
 
 #Update payroll
 def update_payroll_crud(id, employee_contract_hours, employee_rota_hours, employee_worked_hours, employee_lates, employee_early, employee_leaves):
@@ -27,6 +38,42 @@ def update_payroll_crud(id, employee_contract_hours, employee_rota_hours, employ
 
         if employee_leaves:
             payroll.employee_leaves = employee_leaves
+
+        #Calculations
+
+        payroll.employee_hourly_rate = get_hourly_rate(
+            payroll.employee_basic_salary,
+            payroll.employee_contract_hours)
+
+        payroll.employee_over_below = get_over_below(
+            payroll.employee_worked_hours,
+            payroll.employee_contract_hours)
+
+        payroll.employee_score = get_score(
+            payroll.employee_lates,
+            payroll.employee_early,
+            payroll.employee_leaves)
+
+        payroll.total_addition = get_addition(
+            payroll.employee_over_below,
+            payroll.employee_hourly_rate)
+
+        payroll.total_deduction = get_deduction(
+            payroll.employee_over_below,
+            payroll.employee_hourly_rate)
+
+        payroll.total_gross = get_gross(
+            payroll.employee_basic_salary,
+            payroll.total_addition,
+            payroll.total_deduction)
+
+        payroll.total_tax = get_tax(payroll.total_gross)
+
+        payroll.employee_total_net = get_total_net(
+            payroll.total_gross,
+            payroll.total_tax)
+
+        payroll.total_net_orion = get_net_orion(payroll.total_gross)
 
         db.session.commit()
 
